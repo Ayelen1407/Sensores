016912bac13af27df453022dd1bf140e80d8d697
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-08-13 09:59:02 -0300

    Nueva ruta api/sensor usando el simulador Postman

--- servidor/app.py
+++ servidor/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/sensor", methods=['POST'])
+def respuestaSensor():
+    
+    n = request.json["Nombre"]
+    v = request.json["Valor"]
+
+    print (f"El sensor es {n} y el valor que devuelve es {v}")
+    return "ok"
