d254c911f3c66061de4dcff320cab32ec9efd32e
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-08-14 16:43:47 -0300

    Agregue la carpeta Clientes

--- servidor/app.py
+++ servidor/app.py
@@ -12,5 +12,5 @@ def respuestaSensor():
     n = request.json["Nombre"]
     v = request.json["Valor"]
 
-    print (f"El sensor es {n} y el valor que devuelve es {v}")
+    print (f"Nombre del sensor: {n}, valor: {v}")
     return "ok"
